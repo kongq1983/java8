702dfc4a5f72288962862302cb483a04d77e9ab3
kq <kongq1983@yeah.net>
2019-03-16 22:52:23 +0800

    add static method demo

---
+++ src/com/kq/entity/Person.java
@@ -0,0 +1,39 @@
+package com.kq.entity;
+
+/**
+ * Created by qikong on 2019/3/16.
+ */
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
---
+++ src/com/kq/staticdemo/IParse.java
@@ -0,0 +1,10 @@
+package com.kq.staticdemo;
+
+/**
+ * Created by qikong on 2019/3/16.
+ */
+public interface IParse {
+
+    int parse(String str);
+
+}
