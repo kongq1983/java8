216a75d2c1164947fef1b9eed1a81d73f94e9075
king <king@qq.com>
2019-03-21 17:02:55 +0800

    类型转化

--- src/com/kq/entity/Person.java
+++ src/com/kq/entity/Person.java
@@ -8,9 +8,16 @@ public class Person {
     private String firstName;
     private String lastName;
 
+    private int age;
+
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public Person(String firstName,int age){
+        this.firstName = firstName;
+        this.age = age;
     }
 
     public String getFirstName() {
@@ -29,11 +36,20 @@ public class Person {
         this.lastName = lastName;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     @Override
     public String toString() {
         return "Person{" +
                 "firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
+                ", age=" + age +
                 '}';
     }
 }
