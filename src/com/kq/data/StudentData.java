f975d68439bba471c869b2c1f4c3c1b48770febf
king <king@qq.com>
2019-03-22 12:40:43 +0800

    add

--- src/com/kq/data/StudentData.java
+++ src/com/kq/data/StudentData.java
@@ -4,16 +4,19 @@ import com.kq.entity.Student;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class StudentData {
 
     public static List<Student> getStudentList(){
-        int[] ages= {18,16,22,17,20,30,15,28};
+        int[] ages= {18,16,22,17,18,20,30,15,28};
 
 
+        Random r = new Random();
         List<Student> list = new ArrayList();
         for(int age : ages) {
             Student stu = new Student();
+            stu.setId(r.nextInt(100));
             ((Student) stu).setAge(age);
             ((Student) stu).setName("king"+age);
             list.add(stu);
--- src/com/kq/map/ListToMapDemo.java
+++ src/com/kq/map/ListToMapDemo.java
@@ -3,7 +3,11 @@ package com.kq.map;
 import com.kq.data.StudentData;
 import com.kq.entity.Student;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * ListToMapDemo
@@ -15,6 +19,29 @@ public class ListToMapDemo {
 
     public static void main(String[] args) {
         List<Student> list = StudentData.getStudentList();
+
+        //根据名称分组
+        Map<String,List<Student>> map = list.stream().collect(Collectors.groupingBy(Student::getName));
+
+        map.forEach((k,v) ->{
+            System.out.println("key="+k+" v="+v);
+            //取最大值
+            Optional<Student> maxStudnet = v.stream().collect(Collectors.maxBy(Comparator.comparing(Student::getId)));
+            System.out.println("maxStudnet:"+maxStudnet);
+
+        });
+
+
+
+        System.out.println(map);
+
+//        Map<String,Long> map = list.stream().collect(Collectors.groupingBy(Student::getName,Collectors.counting()));
+//        Map<String,Student> map = list.stream().collect(Collectors.groupingBy(Student::getName)
+//                d -> d.collect(Collectors.minBy(Comparator.comparing(Student::getAge))));
+
+//        System.out.println(map);
+
+//        accounts.stream().collect(Collectors.toMap(Account::getId, Account::getUsername));
 
 //        Map<String,Student> map = list.stream().c
 
