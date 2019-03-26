94039d7e56b467edc83473541fac7c724b0796c1
kq <kongq1983@yeah.net>
2019-03-26 20:21:22 +0800

    add consumer demo

--- src/com/kq/entity/Person.java
+++ src/com/kq/entity/Person.java
@@ -20,6 +20,12 @@ public class Person {
         this.age = age;
     }
 
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
     public String getFirstName() {
         return firstName;
     }
