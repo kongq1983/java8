44c7429270334280e854591d17627a55d0ae5d81
king <king@qq.com>
2020-05-26 10:35:18 +0800

    add demo

--- src/com/kq/map/ListToMapDemo.java
+++ src/com/kq/map/ListToMapDemo.java
@@ -26,7 +26,7 @@ public class ListToMapDemo {
             Map<Integer, String> map = list.stream().filter(s -> s.getName() != null).collect(Collectors.toMap(Student::getAge, Student::getName));
         }
         catch(Exception e){
-            System.out.println(e.getMessage());
+            System.out.println("exception:"+e.getMessage());
         }
 
 //        System.out.println(list.stream().filter(t -> t.getName() != null).collect(Collectors.toMap(TestDO::getId, TestDO::getName, (k1, k2) -> k1)));
@@ -34,6 +34,13 @@ public class ListToMapDemo {
         Map<Integer, String> map = list.stream().filter(s -> s.getName() != null).collect(Collectors.toMap(Student::getAge, Student::getName,(k1,k2)-> k1));
         System.out.println(map);
 
+        Map<String,Student> map1 = list.stream().collect(Collectors.toMap(Student::getName,s->s,(k1,k2)-> k1));
+        System.out.println(map1);
+
+        String message = "[%s]计划数量小于销售数量！ 计划数量=%s 销售数量=%s ";
+
+        String mes = String.format(message,"红烧鲫鱼",100,200);
+        System.out.println(mes);
 
     }
 
