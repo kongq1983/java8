cceae0067645c86cb8462168a4e1dec5c288922e
king <king@qq.com>
2020-06-15 17:56:23 +0800

    add demo

---
+++ src/com/kq/hash/HashMapDemo.java
@@ -0,0 +1,14 @@
+package com.kq.hash;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+
+    public static void main(String[] args) {
+
+        HashMap<String,String> map = new HashMap<>();
+        map.put("aa","aa");
+
+    }
+
+}
