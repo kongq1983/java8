c850193f463a657ea33a8fd27273e70fa35cf72e
king <king@qq.com>
2022-01-12 17:14:07 +0800

    add demo

--- src/com/kq/jvm/forname/ClassNameDemo.java
+++ src/com/kq/jvm/forname/ClassNameDemo.java
@@ -10,6 +10,23 @@ public class ClassNameDemo {
     // Class.c
     public static void main(String[] args) throws Exception{
         Class clazz = Class.forName(MyDto.class.getName());
+
+        MyDto.class.getDeclaredMethods();
+        clazz.getClass();
+
+        MyDto1 myDto1 = new MyDto1();
+        MyDto1 myDto2 = new MyDto1();
+
+        // c1 c2 c3 都是同一个
+        Class c1 = myDto1.getClass();
+        Class c2 = myDto2.getClass();
+        Class c3 = MyDto1.class;
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+
+
     }
 
 }
