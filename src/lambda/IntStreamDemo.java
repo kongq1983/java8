28c74eadb41ee01621a5cfebb73a70a5ad1baf72
kong <king@qq.com>
2022-11-04 10:46:12 +0800

    add intstream demo

--- src/lambda/IntStreamDemo.java
+++ src/lambda/IntStreamDemo.java
@@ -1,5 +1,6 @@
 package lambda;
 
+import java.util.OptionalInt;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -11,6 +12,26 @@ import java.util.stream.Stream;
 public class IntStreamDemo {
 
     public static void main(String[] args) {
+        simple();
+        System.out.println("start map-demo");
+        map();
+        System.out.println("e-n-d map-demo");
+
+        System.out.println("start flatmap-demo");
+        flatMap();
+        System.out.println("e-n-d flatmap-demo");
+
+        System.out.println("start filter-demo");
+        filter();
+        System.out.println("e-n-d filter-demo");
+
+
+        System.out.println("start of-demo");
+        of();
+        System.out.println("e-n-d of-demo");
+    }
+
+    public static void main1(String[] args) {
 
         IntStream intStream = IntStream.range(1, 10);
 //        intStream.forEach(System.out::println);  // 不注释，下面使用，会报stream has already been operated upon or closed
@@ -31,7 +52,72 @@ public class IntStreamDemo {
         System.out.println("---------------------------------");
 
         stream1.forEach(System.out::println);
+
+        // 注意range:前面1个数包括  后面1个数不包括
+        IntStream intStream2 = IntStream.range(1, 5); // 1 2 3 4 5
+//        int sum = intStream2.reduce(0, Integer::sum); //
+        int sum = intStream2.reduce(0, Integer::sum); //
+        System.out.println("sum="+sum);
+
+
+//        IntStream intStream3 = IntStream.range(0, 0); // 1 2 3 4 5
+        IntStream intStream3 = IntStream.range(1, 5); // 1 2 3 4 5
+        OptionalInt sumOptional = intStream3.reduce(Integer::sum);
+
+        int sum1 = 0;
+        try {
+            // 这回会报错  IntStream.range(0, 0)
+            sum1 = sumOptional.getAsInt();
+            System.out.println("first sum1=" + sum1);
+        }catch (Exception e) {
+            sum1 = sumOptional.orElse(0);
+            System.out.println("second sum1="+sum1);
+        }
+
+
     }
 
+    public static void simple(){
+
+        // 左闭-右开
+        IntStream intStream = IntStream.range(1, 10);
+        intStream.forEach(System.out::println);
+
+    }
+
+    public static void map(){
+
+        // 左闭-右开
+        IntStream intStream = IntStream.range(1, 10);
+        // n*n
+        intStream.map(e->e*e).forEach(System.out::println);
+
+    }
+
+    public static void flatMap(){
+
+        // 左闭-右开
+        IntStream intStream = IntStream.range(1, 10);
+        // n*n
+        intStream.flatMap(v->IntStream.of(v*v)).forEach(System.out::println);
+
+    }
+
+    public static void filter(){
+
+        // 左闭-右开
+        IntStream intStream = IntStream.range(1, 10);
+        // n*n
+        intStream.filter(e->e%2==0).forEach(System.out::println);
+
+    }
+
+    public static void of(){
+        IntStream intStream = IntStream.of(1,2,3,4,5,6,7,8,9,10);
+        intStream.forEach(System.out::println);
+    }
+
+    // of
+    // IntStream intStream1 = IntStream.of(20, 40, 60, 80, 100, 120, 140);
 
 }
